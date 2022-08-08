7c185025413a78d0a8b0ec51f08818e98737f8cf
Nudelrudel <leon.puricelli@gmail.com>
2022-08-08 21:04:04 +0200

    Check NaiveBayes

--- Fake_or_Real_language_detection.py
+++ Fake_or_Real_language_detection.py
@@ -420,3 +420,4 @@ Naive = naive_bayes.MultinomialNB()
 Naive.fit(Train_X_Tfidf,Train_Y)# predict the labels on validation dataset
 predictions_NB = Naive.predict(Test_X_Tfidf)# Use accuracy_score function to get the accuracy
 print("Naive Bayes Accuracy Score -> ",accuracy_score(predictions_NB, Test_Y)*100)
+#test
